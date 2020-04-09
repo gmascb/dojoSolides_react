f1a0da3c536c5fd1cb9c8d218cf134d56a9ee41b
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2020-04-09 20:09:00 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [lista, setLista] = useState([])
+  const [value, setValue] = useState("")
+
+  useEffect( () => {
+    console.log("Variavel lista mudou !!!", lista);
+  }, [lista]);
+
+  useEffect( () => {
+    console.log("Variavel value mudou !!!", value);
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "">
+        <div style={{display: "flex", flexDirection: "row"}}>
+          
+          <input onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            value={value}/>
+          
+          {value}
+          <button onClick={ () => {
+            setLista([... lista, value])
+            setValue("");
+            }}> Adicionar
+          </button>
+
+        </div>
+      </div>
+
+      <Listagem lista={lista}/>
     </div>
   );
 }
+
+
+
+const Listagem = ( {lista}) => {
+  return lista.map(e => <p> {e} </p>)
+}
+
 
 export default App;
